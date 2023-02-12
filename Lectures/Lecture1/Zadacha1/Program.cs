df6eaf03c5dd34396d2b31cffdb6d19770f0ea87
unknown <ra-lana@mail.ru>
2023-02-12 09:30:17 +0300

    пример применения new Random().Next(min,max) при нахождении суммы двух чисел

---
+++ Lectures/Lecture1/Zadacha1/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA= new Random().Next(1, 10); // При этом numberA будет принимать значения от 1 до 9
+Console.WriteLine(numberA); // Выводим на экран numberA, чтобы мы видели, какие рандомные числа выбрал компьютер
+int numberB= new Random().Next(1, 10); 
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
