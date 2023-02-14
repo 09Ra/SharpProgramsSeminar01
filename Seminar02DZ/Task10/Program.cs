f1dad4a43944d76644f455ff07a8436d3d8c2e04
unknown <ra-lana@mail.ru>
2023-02-14 17:28:21 +0300

    Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ Seminar02DZ/Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.Write("Ввведите трехзначное число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+int result = number / 100;
+
+if (result > 0 && result < 10)
+{
+    number = number % 100;
+    number = number / 10;
+
+    System.Console.WriteLine(number);
+}
+else
+{
+    System.Console.WriteLine("Введенное число не трехзначное.");
+}
