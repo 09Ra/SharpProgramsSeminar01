2b82bcd7d037ccc036cb4463d0d98258a79e8c97
unknown <ra-lana@mail.ru>
2023-02-19 17:01:08 +0300

    Работа с текстом. Замена символов

---
+++ Lectures/Lecture3/Exemple02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить на черточки,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = " - Я думаю, сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; // Инициализация пустой строки
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '*');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(text, 'С', 'с');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+System.Console.WriteLine(newText);
