a03b990498f71a4ad312755bc5c64933b6c6665b
unknown <ra-lana@mail.ru>
2023-02-19 11:46:26 +0300

    Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.

---
+++ Seminar/Seminar03DZ/Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+System.Console.Write("Введите координату X в точке А: ");
+int[] coordsA = new int[3]; 
+coordsA[0] = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите координату Y в точке А: ");
+coordsA[1] = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите координату Z в точке А: ");
+coordsA[2] = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите координату X в точке B: ");
+int[] coordsB = new int[3]; 
+coordsB[0] = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите координату Y в точке B: ");
+coordsB[1] = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите координату Y в точке B: ");
+coordsB[2] = Convert.ToInt32(System.Console.ReadLine());
+
+double distans = Math.Round(Math.Sqrt( (int) Math.Pow((coordsB[0] - coordsA[0]), 2) + (int) Math.Pow((coordsB[1] - coordsA[1]), 2)+(int) Math.Pow((coordsB[2] - coordsA[2]), 2)),2); 
+
+System.Console.WriteLine(distans);
